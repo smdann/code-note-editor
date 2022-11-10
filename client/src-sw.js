96ac77c1f246b353df5e21e2196737525728fb07
SDann <smdann17@gmail.com>
2022-11-10 14:27:19 -0600

    Added StaleWhileRevalidate to the require statement for workbox strategies. In registerRoute function, asset caching is implemented by referencing the destination being sought and verifying it against an item in the paths array. StaleWhileRevalidate is instantiated if a viable route is idendified.

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -26,5 +26,17 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
+// We only register a route if the requested destination is style, script, or worker
+const paths = ["style", "script", "worker"]
+
 // TODO: Implement asset caching
-registerRoute();
+registerRoute( ({ request }) => paths.includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
